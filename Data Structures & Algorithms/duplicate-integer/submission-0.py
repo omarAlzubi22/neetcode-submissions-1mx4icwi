d462bd79eaf07b909a7be7c271d73ceffbe4fa0c
omarAlzubi22 <omar.alzubi@uconn.edu>
2026-04-02 22:50:34 -0400

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        hash
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] == nums[j]:
+                    return True
+                else:
+                    j+=1
+        return False
+
+
+
+
+        
+        
